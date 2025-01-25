de2210b6ebb41d62420d65e0da0b2d2430020b9f
Kenneth Chidozie <agbokennethchidoziem@gmail.com>
2025-01-25 00:11:54 -0800

    initial commit

---
+++ src/main/java/com/doziem/jamTesSystem/JamTesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.doziem.jamTesSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamTesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamTesSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doziem/jamTesSystem/JamTesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doziem.jamTesSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamTesSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
